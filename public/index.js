e21837f5a655e59ff596c896b70206655515b10a
Louis-Marie Michelin <lmichelin@outlook.fr>
2019-06-21 14:02:58 +0200

    #2 see webcam stream in index.html page

---
+++ public/index.js
@@ -0,0 +1,16 @@
+const startStream = async () => {
+  const videoElement = document.getElementById("video")
+
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    })
+
+    videoElement.srcObject = stream
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+startStream()
