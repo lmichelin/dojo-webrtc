84d1b3dbbd146c401e91d2b082ac43b08a47d835
Louis-Marie Michelin <lmichelin@outlook.fr>
2019-07-07 23:01:19 +0200

    #15 send file (works only on Firefox)

--- public/index.js
+++ public/index.js
@@ -1,5 +1,29 @@
 const socket = io.connect("/")
 
+const sendFile = peer => {
+  const fileInput = document.getElementById("fileInput")
+
+  if (fileInput.files && fileInput.files[0]) {
+    peer.send(fileInput.files[0])
+    return
+  }
+
+  alert("Please choose a file!")
+}
+
+const downloadBlob = blob => {
+  let url = URL.createObjectURL(blob)
+  let aElement = document.createElement("a")
+  aElement.setAttribute("href", url)
+  let filename = new Date().toISOString().split(".")[0]
+  aElement.setAttribute("download", filename)
+  aElement.style.display = "none"
+  document.body.appendChild(aElement)
+  aElement.click()
+  document.body.removeChild(aElement)
+  URL.revokeObjectURL(url)
+}
+
 socket.on("offer", signal => {
   startStream(false, signal)
 })
@@ -27,6 +51,15 @@ const startStream = async (isInitiator, offer) => {
       const videoElement = document.getElementById("video")
       videoElement.srcObject = stream
     })
+
+    peer.on("data", data => {
+      const blob = new Blob([new Uint8Array(data)]) // https://stackoverflow.com/questions/44147912/arraybuffer-to-blob-conversion?answertab=votes#tab-top
+      downloadBlob(blob)
+    })
+
+    const sendButton = document.getElementById("sendButton")
+
+    sendButton.addEventListener("click", () => sendFile(peer))
   } catch (error) {
     console.error(error)
   }
